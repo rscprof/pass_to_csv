36f0246a22e4bd4143bcaba4ca56e7ea41e060a6
Glusker Alexander <rsc@rsc.pp.ru>
2017-11-28 20:08:58 +0300

    Pylint

--- pass_to_csv/__init__.py
+++ pass_to_csv/__init__.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python
+#/usr/bin/python
+"""
+Export from password.store to csv file
+"""
 
 import os
 import logging
@@ -17,8 +20,11 @@ class PrepareFilter:
         self.column7 = column7
 
 
-    def run(self, listPass):
-        return [listPass[0], "", listPass[1], "", "", self.column6, self.column7]
+    def run(self, listpass):
+        """
+        run filter
+        """
+        return [listpass[0], "", listpass[1], "", "", self.column6, self.column7]
 
 class DotFilters:
     """
@@ -28,20 +34,41 @@ class DotFilters:
         self.filter1 = filter1
         self.filter2 = filter2
 
-    def run(self, listPass):
-        return self.filter2.run(self.filter1.run(listPass))
+    def run(self, listpass):
+        """
+        run filter
+        """
+        return self.filter2.run(self.filter1.run(listpass))
 
 class BaseChainFilter(object):
+    """
+    This is the abstract base class for
+    chain of responsibility of filters
+
+    Only one filter must be used
+
+    The method runinside of it must return not None
+
+    """
     def __init__(self, chain):
         self.chain = chain
-    def run(self, listPass):
-        result = self.runInside(listPass)
-        if result is None:
-            return self.chain.run(listPass)
-        else:
-            return result
 
-class PrefixChainFilter (BaseChainFilter):
+    def runinside(self, listpass):
+        """
+        This is the method only for override
+        """
+        return listpass
+
+    def run(self, listpass):
+        """
+        run filter
+        """
+        result = self.runinside(listpass)
+        if result is None:
+            return self.chain.run(listpass)
+        return result
+
+class PrefixChainFilter(BaseChainFilter):
     """
     filter that searches prefix in second section before /
     add move in into other group
@@ -50,101 +77,107 @@ class PrefixChainFilter (BaseChainFilter):
         super(PrefixChainFilter).__init__(chain)
         self.prefix = prefix
 
-    def runInside(self, listPass):
-        slashIndex = listPass[0].find('/')
-        if slashIndex != -1:
-            firstPart = listPass[0][:slashIndex]
-            if firstPart == self.prefix:
-                logging.debug('Find prefix '+self.prefix+' into '+listPass[0])
-                listPass[0] = listPass[0][slashIndex+1:]
-                listPass[5] = firstPart
-                return listPass
+    def runinside(self, listpass):
+        slashindex = listpass[0].find('/')
+        if slashindex != -1:
+            firstpart = listpass[0][:slashindex]
+            if firstpart == self.prefix:
+                logging.debug('Find prefix '+self.prefix+' into '+listpass[0])
+                listpass[0] = listpass[0][slashindex+1:]
+                listpass[5] = firstpart
+                return listpass
 
 
-class UsernameChainFilter (BaseChainFilter):
+class UsernameChainFilter(BaseChainFilter):
     """
     filter that searches username in last section after /
     add move it into other column
     """
-    def __init__(self, chain, usernameSubstring):
+    def __init__(self, chain, usernamesubstring):
         super(UsernameChainFilter).__init__(chain)
-        self.usernameSubstring = usernameSubstring
+        self.usernamesubstring = usernamesubstring
 
-    def runInside(self, listPath):
-        slashIndex = listPath[0].rfind('/')
-        if slashIndex != -1:
-            lastPart = listPath[0][slashIndex+1:]
-            if lastPart.find(self.usernameSubstring) != -1:
-                logging.debug('Find username '+self.usernameSubstring+' into '+lastPart)
-                listPath[1] = lastPart
-                listPath[0] = listPath[0][:slashIndex]
-                return listPath
+    def runinside(self, listpass):
+        """
+        find usernameSubstring in last part of listpath[0] and if it exists then
+        username moves to other item of array
+        """
+        slashindex = listpass[0].rfind('/')
+        if slashindex != -1:
+            lastpart = listpass[0][slashindex+1:]
+            if lastpart.find(self.usernamesubstring) != -1:
+                logging.debug('Find username '+self.usernamesubstring+' into '+lastpart)
+                listpass[1] = lastpart
+                listpass[0] = listpass[0][:slashindex]
+                return listpass
 
 
-class DropSlashFilter (BaseChainFilter):
+class DropSlashFilter(BaseChainFilter):
     """
     filter that deletes / in front of string
     """
-    def runInside(self, listPath):
-        if listPath[0][0:1] == '/' and listPath[0].find('/', 1)==-1:
-            listPath[0] = listPath[0][1:]
-            return listPath
-        else:
-            return None
+    def runinside(self, listpass):
+        if listpass[0][0:1] == '/' and listpass[0].find('/', 1) == -1:
+            listpass[0] = listpass[0][1:]
+            return listpass
 
 
 
-class URLWithoutUsernameChainFilter (BaseChainFilter):
+class URLWithoutUsernameChainFilter(BaseChainFilter):
     """
     filter that converts ["/xxx.yyy", "", "zzz", "", ""] into ["xxx.yyy", "", "zzz", "xxx.yyy", ""]
     """
-    def runInside(self, listPath):
-        name = listPath[0]
-        if (name[0:1] == '/')and(name[1:].find('.')>0):
-            listPath[0] = name[1:]
-            listPath[3] = name[1:]
-            return listPath
-        else:
-            return None
+    def runinside(self, listpass):
+        name = listpass[0]
+        if (name[0:1] == '/')and(name[1:].find('.') > 0):
+            listpass[0] = name[1:]
+            listpass[3] = name[1:]
+            return listpass
 
 
-class URLChainFilter (BaseChainFilter):
+class URLChainFilter(BaseChainFilter):
     """
     filter that convert ["xxx/yyy", "", "zzz", "", ""] into ["yyy", "yyy", "zzz", "xxx", ""]
     if it is imposiible then filter start chain filter
     """
-    def runInside(self, listPass):
-        name = listPass[0]
-        splitFirstPart = name.split('/')
-        if (len(splitFirstPart) != 2 or splitFirstPart[0] == "" or splitFirstPart[0].find('.')==-1):
+    def runinside(self, listpass):
+        name = listpass[0]
+        splitfirstpart = name.split('/')
+        if len(splitfirstpart) != 2 or splitfirstpart[0] == "" or splitfirstpart[0].find('.') == -1:
             return None
-        listPass[0] = splitFirstPart[0]
-        listPass[1] = splitFirstPart[1]
-        listPass[3] = splitFirstPart[0]
-        return listPass
+        listpass[0] = splitfirstpart[0]
+        listpass[1] = splitfirstpart[1]
+        listpass[3] = splitfirstpart[0]
+        return listpass
 
 
 
-class identityFunctor:
+class IdentityFunctor:
     """
         functor that return his argument
     """
     def run(self, arg):
+        """
+        identity function :-)
+        """
         return arg
 
-class filterWithFunctor:
+class FilterWithFunctor:
     """
     This is class-adapter of csv, that use functor to do
     with array (name, password)
     """
-    def __init__ (self, parentcsv, functor):
+    def __init__(self, parentcsv, functor):
         self.parentcsv = parentcsv
         self.functor = functor
 
     def writerow(self, arg):
+        """
+        save the arg, that went through function
+        """
         self.parentcsv.writerow(self.functor.run(arg))
 
-class filterExample:
+class FilterExample:
     """
     This is an example of class-adapter for csv
     """
@@ -152,44 +185,54 @@ class filterExample:
         self.parentcsv = parentcsv
 
     def writerow(self, arg):
+        """
+        Only save to csv
+        """
         self.parentcsv.writerow(arg)
 
 def main():
+    """
+    Main function in program
+    """
     home = os.environ['HOME']
     parser = argparse.ArgumentParser(
-        usage = "pass_to_csv [-h] [-v] -6 General -7 Pass [-u user] ... [-u user] [-p prefix] [-p prefix]",
-                                     description = "Export from passwordstore.org storage to csv file")
-    parser.add_argument('-6', '--column6', help = 'default value for column 6')
-    parser.add_argument('-7', '--column7', help = 'default value for column 7')
-    parser.add_argument('-u', '--username', help = 'substring for check username existing', action = 'append')
+        usage=
+        "pass_to_csv [-h] [-v] -6 General -7 Pass [-u user] ... [-u user] [-p prefix] [-p prefix]",
+        description="Export from passwordstore.org storage to csv file")
+    parser.add_argument('-6', '--column6', help='default value for column 6')
+    parser.add_argument('-7', '--column7', help='default value for column 7')
+    parser.add_argument('-u', '--username',
+                        help='substring for check username existing', action='append')
 
-    parser.add_argument('-v', '--verbose', help = 'show verbose information', action = 'store_true')
-    parser.add_argument('-p', '--prefix', help = 'prefix of name for selecting groups', action = 'append')
+
+    parser.add_argument('-v', '--verbose', help='show verbose information', action='store_true')
+    parser.add_argument('-p', '--prefix',
+                        help='prefix of name for selecting groups', action='append')
 
     args = parser.parse_args()
     if args.verbose:
-        logging.basicConfig(stream = sys.stderr, level = logging.DEBUG)
+        logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
     directory_prefix = home+'/.password-store'
     try:
         csvfile = csv.writer(sys.stdout)
-        chain = DropSlashFilter (identityFunctor())
+        chain = DropSlashFilter(IdentityFunctor())
         #test for username
-        if (args.username):
-            for u in args.username:
-                chain = UsernameChainFilter(chain, u)
-        if (args.prefix):
-            for p in args.prefix:
-                chain = PrefixChainFilter(chain, p)
+        if args.username:
+            for username in args.username:
+                chain = UsernameChainFilter(chain, username)
+        if args.prefix:
+            for prefix in args.prefix:
+                chain = PrefixChainFilter(chain, prefix)
         chain = URLChainFilter(chain)
         chain = URLWithoutUsernameChainFilter(chain)
 
 
         scandir("", directory_prefix,
-                filterWithFunctor(csvfile,
-                DotFilters(PrepareFilter(args.column6, args.column7), chain)))
+                FilterWithFunctor(csvfile,
+                                  DotFilters(PrepareFilter(args.column6, args.column7), chain)))
 
     except FileNotFoundError as err:
-        if (err.filename == directory_prefix):
+        if err.filename == directory_prefix:
             print("Password storage doesn't exist")
         else:
             raise err
@@ -211,24 +254,24 @@ def scandir(directory, directory_prefix, writer):
     path = directory_prefix
     if directory != '':
         path += '/'+directory
-    for fileStruct in os.scandir(path):
-        if fileStruct.is_dir():
-            if fileStruct.name != '.git':
-                logging.debug('scanning directory '+fileStruct.name)
+    for filestruct in os.scandir(path):
+        if filestruct.is_dir():
+            if filestruct.name != '.git':
+                logging.debug('scanning directory '+filestruct.name)
                 newdirectory = directory
                 if directory != '':
                     newdirectory += '/'
-                newdirectory += fileStruct.name
+                newdirectory += filestruct.name
                 result = result + scandir(newdirectory, directory_prefix, writer)
-        elif fileStruct.is_file() and fileStruct.name[-4:] == '.gpg':
-            logging.debug('scanning file '+fileStruct.name)
-            name = directory+'/'+fileStruct.name[:-4]
-            password = subprocess.run(['pass', name], stdout = subprocess.PIPE)
+        elif filestruct.is_file() and filestruct.name[-4:] == '.gpg':
+            logging.debug('scanning file '+filestruct.name)
+            name = directory+'/'+filestruct.name[:-4]
+            password = subprocess.run(['pass', name], stdout=subprocess.PIPE)
             if password.returncode == 0:
                 writer.writerow([name, password.stdout.rstrip().decode("utf-8")])
             else:
                 sys.exit('Cannot get the password for ' +
-                         directory + '/' + fileStruct.name)
+                         directory + '/' + filestruct.name)
     return result
 
 
